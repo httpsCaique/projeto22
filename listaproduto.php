1230edeaafc6d18990494360d5a1eb2dfe1dd0ba
httpsCaiqueArruda <https.caique@hotmail.com>
2023-07-24 08:36:25 -0300

    Update listaproduto.php

--- listaproduto.php
+++ listaproduto.php
@@ -1,63 +1,49 @@
 <?php
-
 include("conectadb.php");
+
 session_start();
-$nomeusuario = $_SESSION["nomeusuario"];
+$nomeusuario = $_SESSION['nomeusuario'];
 
 $sql = "SELECT * FROM produtos WHERE pro_ativo = 's'";
 $retorno = mysqli_query($link, $sql);
 $ativo = "s";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $ativo = $_POST['ativo'];
 
-
-    if($ativo == 's')
-    {
-        $sql = "SELECT * FROM produtos WHERE pro_ativo = 's' ";
+    #VALIDA SE PRODUTO EXISTE
+    if($ativo == 's'){
+        $sql = "SELECT * FROM produtos WHERE pro_ativo = 's'";
         $retorno = mysqli_query($link, $sql);
     }
-    else
-    {
-        $sql = "SELECT * FROM produtos WHERE pro_ativo = 'n' ";
+    else{
+        $sql = "SELECT * FROM produtos WHERE pro_ativo = 'n'";
         $retorno = mysqli_query($link, $sql);
-
     }
-
-
 }
 
 
-
-
-
-
-
-
-
-
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/estiloadm.css">
+    <link rel="stylesheet" href="css/estiloadm.css">
     <title>LISTA PRODUTOS</title>
 </head>
+
 <body>
-<div>
-        <!--O MENU GERAL DA APLICAÇÃO -->
+    <!-- NOSSO MENU GLOBAL -->
+    <div>
         <ul class="menu">
             <li><a href="cadastrausuario.php">CADASTRA USUARIO</a></li>
             <li><a href="listausuario.php">LISTA USUARIO</a></li>
             <li><a href="cadastraproduto.php">CADASTRA PRODUTO</a></li>
-            <li><a href="cadastracliente.php">CADASTRA CLIENTE</a></li>
             <li><a href="listaproduto.php">LISTA PRODUTO</a></li>
-            <li><a href="listaclientes.php">LISTA CLIENTE</a></li>
-            <li><a href="alterarclientes.php">ALTERA CLIENTE</a></li>
+            <li><a href="listacliente.php">LISTA CLIENTE</a></li>
             <li class="menuloja"><a href="logout.php">SAIR</a></li>
             <?php
             #ABERTO O PHP PARA VALIDAR SE A SESSÃO DO USUARIO ESTÁ ABERTA
@@ -76,18 +62,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
             ?>
         </ul>
     </div>
-
+    <!-- FIM MENU GLOBAL -->
 
     <div>
         <form action="listaproduto.php" method="post">
+            <input type="radio" name="ativo" class="radio" value="s" required onclick="submit()" <?= $ativo == 's' ? "checked" : "" ?>>ATIVOS
 
-        <input type="radio" name="ativo" class="radio" value="s" required 
-            onclick="submit()" <?=$ativo =='s'?"checked":""?>>ATIVOS
-
-            <input type="radio" name="ativo" class="radio" value="n" required 
-            onclick="submit()" <?=$ativo =='n'?"checked":""?>>INATIVOS
-
-
+            <input type="radio" name="ativo" class="radio" value="n" required onclick="submit()" <?= $ativo == 'n' ? "checked" : "" ?>>INATIVOS
         </form>
         <div class="container">
             <table border="1">
@@ -100,34 +81,37 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
                     <th>PREÇO</th>
                     <th>IMAGEM</th>
                     <th>ALTERAR</th>
-                    <th>ATIVO?</th>      
+                    <th>ATIVO?</th>
                 </tr>
-                <?php 
-                while($tbl = mysqli_fetch_array($retorno))
-                {
+                <?php
+                    while($tbl = mysqli_fetch_array($retorno)){
+                        ?>
+                    
+                    <tr>
+                        <td><?=$tbl[0]?></td> <!-- COLETA ID PRODUTO COLUNA 0-->
+                        <td><?=$tbl[1]?></td> <!-- COLETA NOME PRODUTO COLUNA 1-->
+                        <td><?=$tbl[2]?></td> <!-- COLETA DESCRICAO PRODUTO COLUNA 2-->
+                        <td><?=$tbl[3]?></td> <!-- COLETA QUANTIDADE PRODUTO COLUNA 3-->
+                        <!-- CONVERTE PONTO EM VIRGULA-->
+                        <td>R$ <?=number_format($tbl[4],2,',','.')?></td>
+                        <td>R$ <?=number_format($tbl[5],2,',','.')?></td>
+                        <!-- VEM DO BANCO A IBAGEM EM BASE64 O QUE FAZER?? -->
+                        <!-- DECRIPTAR O BASE64 TRAZENDO A IBAGEM -->
+                        <td><img src="data:image/jpeg;base64,<?=$tbl[7]?>" width="100" height="100"></td>
+                        <!-- BOTÃO ALTERAR PRODUTO -->
+                        <td>   
+                            <a href="alteraproduto.php?id=<?= $tbl[0]?>">
+                            <input type="button" value="ALTERAR">
+                        </td>
+                        <td><?= $check = ($tbl[6] == 's')?"SIM":"NÃO"?></td>
+                    </tr>
+                    <?php
+                    }
                     ?>
-                    
-
-                <tr>
-                    <td><?=[0]?></td>
-                    <td><?=[1]?></td>
-                    <td><?=[2]?></td>
-                    <td><?=[3]?></td>
-                    <td>R$<?=number_format($tbl[4],2,',','.')?></td>
-                    <td>R$<?=number_format($tbl[5],2,',','.')?></td>
-                    <td><img src="data:image/jpeg;base64, <?=[7]?>" width="100" height="100"></td>
-                    <td><a href="alteraproduto.php?id=<?=[0]?>"><input type="button" value="ALTERAR"></td>
-                    <td><?= $check = ($tbl[6]=='s')?"SIM":"NÃO"?></td>
-                    
-
-
-                </tr>
-                <?php 
-                }
-                ?>
             </table>
         </div>
     </div>
-    
+
 </body>
+
 </html>
